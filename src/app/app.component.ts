96732365419440779e7a3d72724ef1c0765ac504
Sayooj V R <sayoojvylappully@gmail.com>
2020-01-25 14:23:36 +0530

    arabic support added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Inject,
+  LOCALE_ID,
+  HostBinding,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'localization-ng';
+
+  @HostBinding('attr.dir') dir = 'ltr';
+
+  constructor(@Inject(LOCALE_ID) private locale: string) {}
+
+  ngOnInit() {
+    if (this.locale.startsWith('ar')) {
+      this.dir = 'rtl';
+    }
+  }
 }
